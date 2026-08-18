30d983cb8440354b4f5aa7c5a279987b5cbe0fb1
THÁI VĂN THI <thaivanthi2005@gmail.com>
2026-08-18 21:57:44 +0700

    feat(auth): add company account model

---
+++ model/account-company.model.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    companyName: String,
+    email: String,
+    password: String,
+    city: String,
+    address: String,
+    companyModel: String,
+    companyEmployees: String,
+    workingTime: String,
+    workOvertime: String,
+    description: String,
+    logo: String,
+    phone: String,
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+const AccountCompany = mongoose.model('AccountCompany', schema, "accounts-company");
+
+export default AccountCompany;
