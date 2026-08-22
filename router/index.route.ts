07e528469a1cc30426fc0fc7fbf82e562255d67a
THÁI VĂN THI <thaivanthi2005@gmail.com>
2026-08-22 23:52:46 +0700

    feat(city): add city data and selection API

--- router/index.route.ts
+++ router/index.route.ts
@@ -2,13 +2,14 @@ import { Router } from "express";
 import userRoutes from "./user.route";
 import checkRoutes from "./auth.route";
 import companyRoute from "./company.route";
-
+import cityRoutes from "./city.route";
 
 const router = Router();
 
 router.use("/user", userRoutes);
 router.use("/auth", checkRoutes);
 router.use("/company",companyRoute);
+router.use('/city', cityRoutes);
 
 
 export default router;
