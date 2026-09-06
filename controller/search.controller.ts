db083c8a75f9cba2c4e9d7dbae9dfa4ac7344b03
thaivanthi2005 <thaivanthi2005@gmail.com>
2026-09-06 09:52:50 +0700

    feat : filter jobs by workingForm

--- controller/search.controller.ts
+++ controller/search.controller.ts
@@ -58,7 +58,10 @@ export const search = async (req: Request, res: Response) => {
       find.position = req.query.position;
     }
 
-    
+    //workingForm
+    if(req.query.workingForm){
+      find.workingForm = req.query.workingForm;
+    }
     const jobs = await Job.find(find).sort({createdAt: "desc"})
 
     for(const item of jobs){
